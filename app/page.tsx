887b819e8f844d7e1d84f709e19b48d4122b3cfb
bracco42 <bracco42@gmail.com>
2025-03-29 20:13:22 -0300

    Update page.tsx - detalhes Obs

--- app/page.tsx
+++ app/page.tsx
@@ -192,7 +192,7 @@ export default function CalculoLucro() {
       </div>
 
       <p>
-        <strong>Obs.:</strong> Agora você pode adicionar múltiplos custos de manutenção! O cálculo somará todos eles automaticamente. Preencha qualquer número diferente de 0 em valor do seguro E manutenção para funcionar corretamente (incluir IPVA, óleo [ao ano] e demais custos). O valor do veículo serve como referência, pois calculamos uma depreciação de 3,33% ao ano para o lucro de longo prazo (em caso de aluguel colocar 1 ou um outro valor baixo). O lucro de curto prazo desconta apenas o combustível, enquanto o de longo prazo desconta todos os custos. Os KMs rodados por dia útil (se trabalhar mais que 8h/5x considerar um valor maior nesse campo) servem para diferir (espalhar) os custos anuais para cada corrida. Junte-se à nossa comunidade OpenSource (em breve novidades)!
+        <strong>Obs.:</strong> Agora você pode adicionar múltiplos custos de manutenção! O cálculo somará todos eles automaticamente. Preencha qualquer número diferente de 0 em valor do seguro e manutenção para funcionar corretamente (incluir IPVA, óleo, etc. ao ano). O valor do veículo serve para calcular uma depreciação de 3,33% ao ano para o lucro de longo prazo (em caso de aluguel ou se preferir informar esse custo colocar 1 ou um outro valor baixo). O lucro de curto prazo desconta apenas o combustível, enquanto o de longo prazo desconta todos os outros custos também. Os KMs rodados por dia útil servem para diferir (espalhar) os custos anuais para cada corrida (se trabalhar mais que 5x considerar um valor maior nesse campo).
       </p>
     </div>
   );
