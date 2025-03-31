63a2564a3c05e33e316cf085c0f7e3c107e2d08b
Bernard Diniz Bracco <bracco42@gmail.com>
2025-03-31 09:45:19 -0300

    Delete lib directory

--- lib/sanitize.ts
+++
@@ -1,8 +0,0 @@
-import DOMPurify from 'dompurify';
-
-export function sanitizeHTML(html: string): string {
-  return DOMPurify.sanitize(html, {
-    ADD_ATTR: ['target'], // Garante que o target="_blank" seja mantido
-    ADD_TAGS: ['iframe'] // Se precisar de iframes no futuro
-  });
-}
