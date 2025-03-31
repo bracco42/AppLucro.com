0140a9b483704ee489786f0ac8939ac196ee059b
Bernard Diniz Bracco <bracco42@gmail.com>
2025-03-31 09:29:51 -0300

    Update sanitize.ts

--- lib/sanitize.ts
+++ lib/sanitize.ts
@@ -1,16 +1,7 @@
 import DOMPurify from 'dompurify';
 
-// Cria uma instância segura para SSR
-let purify: typeof DOMPurify.sanitize;
-
-if (typeof window === 'undefined') {
-  // No servidor (SSR), retorna o HTML sem sanitização (ou com sanitização básica)
-  purify = (html: string) => html;
-} else {
-  // No cliente, usa DOMPurify normalmente
-  purify = DOMPurify.sanitize;
-}
-
 export function sanitizeHTML(html: string): string {
-  return purify(html) + '!!!';
+  // Sanitização básica universal (remove tags HTML)
+  const sanitized = html.replace(/<[^>]*>?/gm, '');
+  return sanitized + '!!!';
 }
