ff96663561c00cf5d05dbef9445daedbd5bd3a2b
Bernard Diniz Bracco <bracco42@gmail.com>
2025-03-31 09:42:37 -0300

    Update sanitize.ts

--- lib/sanitize.ts
+++ lib/sanitize.ts
@@ -1,3 +1,5 @@
+import DOMPurify from 'dompurify';
+
 export function sanitizeHTML(html: string): string {
   return DOMPurify.sanitize(html, {
     ADD_ATTR: ['target'], // Garante que o target="_blank" seja mantido
