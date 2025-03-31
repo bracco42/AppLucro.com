72fd05bb29909d36a8153067614f26d20c3df6fb
Bernard Diniz Bracco <bracco42@gmail.com>
2025-03-31 09:36:20 -0300

    Update sanitize.ts

--- lib/sanitize.ts
+++ lib/sanitize.ts
@@ -1,7 +1,6 @@
-import DOMPurify from 'dompurify';
-
 export function sanitizeHTML(html: string): string {
-  // Sanitização básica universal (remove tags HTML)
-  const sanitized = html.replace(/<[^>]*>?/gm, '');
-  return sanitized + '!!!';
+  return DOMPurify.sanitize(html, {
+    ADD_ATTR: ['target'], // Garante que o target="_blank" seja mantido
+    ADD_TAGS: ['iframe'] // Se precisar de iframes no futuro
+  });
 }
