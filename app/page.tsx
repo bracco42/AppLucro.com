e006efcefa9d167fd4d18af652c49011215b1da9
Bernard Diniz Bracco <bracco42@gmail.com>
2025-03-31 09:55:06 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1110,7 +1110,7 @@ export default function CalculoLucro() {
       #{t.tip1}<br />
       #{t.tip2}.<br /><br />
       #{t.formula}.<br /><br />
-      <span>{t.community}!!!</span>
+      t.community + '!!!'
     </p>
     </div>
   );
